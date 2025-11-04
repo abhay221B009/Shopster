d3fd883f53464e178aef3194cb20d20130ad2737
Abhay Raj Chauhan <115731719+abhay221B009@users.noreply.github.com>
2025-11-04 09:27:46 +0530

    Update CartItems.jsx

--- src/Components/CartItems/CartItems.jsx
+++ src/Components/CartItems/CartItems.jsx
@@ -94,7 +94,7 @@ const CartItems = () => {
           <div>
             <div className="cartitems-total-item">
               <p>Subtotal</p>
-              <p>${getTotalCartAmount()}</p>
+              <p>Rs{getTotalCartAmount()}</p>
             </div>
             <hr />
             <div className="cartitems-total-item">
@@ -104,7 +104,7 @@ const CartItems = () => {
             <hr />
             <div className="cartitems-total-item">
               <h3>Total</h3>
-              <h3>${getTotalCartAmount()}</h3>
+              <h3>Rs{getTotalCartAmount()}</h3>
             </div>
           </div>
           <button onClick={handleRazorpayPayment}>PROCEED TO CHECKOUT</button>
